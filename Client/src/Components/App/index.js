6c7348cbb048bf462770ca842fa24450bf2d2ede
Char J <CharJ2889@gmail.com>
2021-03-24 12:46:03 +0000

    front

--- Client/src/Components/App/index.js
+++ Client/src/Components/App/index.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react"
+//import React, { useEffect, useState } from "react"
+import React from "react"
 import './App.css';
 
 function App() {
+
+  //const []
   return (
     <div className="App">
+      <input className="search-bar" type= "text"/> 
       Console.blob("cohort5 yes!!!")
     </div>
   );
