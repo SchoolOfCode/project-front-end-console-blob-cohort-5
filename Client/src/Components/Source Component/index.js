b3de133e1b7f194d68ac000ff9cbeb882fdeeff6
Mafe Sanchez <mafesanchez0326@gmail.com>
2021-03-26 10:46:06 +0000

    source component created

--- Client/src/Components/Source Component/index.js
+++ Client/src/Components/Source Component/index.js
@@ -1,9 +1,11 @@
-import React from react;
+import React from 'react';
+import css from './sourceComponent.module.css';
+import cn from 'classnames';
 
 
-function SourceComponent({data}){
+function SourceComponent({data, color, size}){
     return(
-        <div>
+        <div classname={cn(css[color], css[size])}>
         <p>{data.source}</p>
         <p>{data.lastupdated}</p>
         </div>
