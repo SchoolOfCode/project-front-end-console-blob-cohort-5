60e286e770f99e90a3ecd617d8cf5289b34dfef2
craig2812 <craig2812@gmail.com>
2021-03-30 11:45:23 +0100

    Refactor new API into Travel Restrictions Display component

--- src/Components/TravelRestrictionsDisplayComponent/index.js
+++ src/Components/TravelRestrictionsDisplayComponent/index.js
@@ -1,37 +1,48 @@
-// import React from 'react';
-// import css from './travelRestrictDisplay.module.css';
-// import cn from 'classnames'
-
-// function TravelRestrictionsDisplay({ countryCode, color, size, imgSize }) {
+import React from 'react'
+import CountryCodesList from "./countriesCodesList";
 
 
-// const [countryData, setCountryData]= useState(""); 
+function TravelRestrictionsDisplay() {
 
-// let BACKEND_URL = `https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions`
+  const[countryCode, setCountryCode] = useState("POL")
 
-// useEffect(() => {
-//   async function fetchCountryData(countryCode) {
-//     let response = await fetch(`${BACKEND_URL}/POL/2021-03-26`);
-//     let data = await response.json();
-//     setCountryData(data);
-//     console.log(data);
+  // const [countryData, setCountryData]= useState(""); 
 
-//     fetchCountryData(countryCode)
-//   }
-// }, [countryCode])
+let BACKEND_URL = `https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions`
+let country = useFetch(`${BACKEND_URL}/${countryCode}/2021-03-10`,[countryCode])
 
 
+console.log(country)
+// let policyType = country.policyActions[8].policy_type_display
 
-//   return (
-//     <div className={cn(css[color], css[size])}>
-//       <h1>{data.countryName}</h1>
-//       <p>{data.entryRestrictions}</p>
-//       <p>{data.quarantineRestirctions}</p>
-//       <img className={cn(css[imgSize])} src={data.imgSrc} alt="Country Flag" />
-//     </div>
-//   );
-// }
+// if (policyType=!undefined){
+//   console.log(policyType)
+// } 
+
+function handleChange(e){
+  setCountryCode(e.target.value)
+}
+
+  return (
+    <div>
+      <CountryCodesList handleChange={handleChange}/>
+
+<div style={{width:"50%"}}>
+  <p>{`Confirmed cases: ${country.stringencyData.confirmed}`}</p>
+  {/* <p>{`Workplace Restrictions: ${country.policyActions[1].policy_value_display_field}`}</p> */}
+  {/* <p>{`International Travel Restrictions: ${country.policyActions[7].policy_value_display_field}`}</p> */}
+  {/* <p>{`Notes: ${country.policyActions[7].notes}`}</p> */}
+
+</div>
+    </div>
+  )
+}
+
+export default TravelRestrictionsDisplay;
 
 
 
-// export default TravelRestrictionsDisplay;
+
+
+
+
