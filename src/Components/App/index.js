200cc9d631dcd9d366c8b5d115a1d2d8e215d10f
Steve Cadden <s.p.cadden@gmail.com>
2021-03-30 10:50:14 +0100

    making progress

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,61 +1,41 @@
-import React from 'react';
-import Button from '../ButtonComponent/index.js';
-import Navbar from '../Navbar/index.js';
-import UKPage from '../UKMainPageComponent';
+import React from "react";
+import Button from "../ButtonComponent/index.js";
+import Navbar from "../Navbar/index.js";
+import UKPage from "../UKMainPageComponent";
 import WorldPage from "../WorldMainPageComponent";
-import WwTracker from "../WorldWideCovidTracker";
+import WwTracker from "../WorlWideCovidTracker";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-
-
-  function App() {
-
-    return (
-      <Router>
+function App() {
+  return (
+    <Router>
       <div>
-            <nav>
-                
-                                  
-               
-                
-                    <Link to="/" >
-                        Home
-                </Link>
-                    <Link to="/UKPage" >
-                        UK
-                    </Link>
-                    <Link to="/WorldPage" >
-                        Abroad
-                    </Link>
-                    <Link to="/WwTracker" >
-                        World Stats
-                </Link>
-                
-            </nav>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/UKPage">UK</Link>
+          <Link to="/WorldPage">Abroad</Link>
+          <Link to="/WwTracker">World Stats</Link>
+        </nav>
 
-        
-          
-        
         <Switch>
-        
-          <Route path="/UKPage"><UKPage /> </Route>
-          <Route path="/WorldPage"><WorldPage /> </Route>
-          
-          <Route path="/WwTracker"><WwTracker /> </Route>
-          <Route path="/"><App /> </Route>
+          <Route path="/UKPage">
+            <UKPage />{" "}
+          </Route>
+          <Route path="/WorldPage">
+            <WorldPage />{" "}
+          </Route>
+
+          <Route path="/WwTracker">
+            <WwTracker />{" "}
+          </Route>
+          <Route path="/">
+            <App />{" "}
+          </Route>
         </Switch>
-        </div>
-        
-</Router>
-
-
-
-
-          {/* {/* <Button variant="large" text="Uk" />
-          <Button variant="large" text="Abroad" /> */}
-           */}
-
-
-
+        <Button variant="large" text="Abroad" />
+        <Button variant="large" text="Uk" />
+      </div>
+    </Router>
   );
 }
 
