f1465f6b146590d5cb08160aef60827ebfc11923
Char J <CharJ2889@gmail.com>
2021-03-31 22:21:57 +0100

    Anitmated logo

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, NavLink, Bars, NavMenu, } from './NavbarElements.js';
 
-import logo from "./logofinal.png";
+import logo from "./animationlogo.gif";
 
 
 const Navbar = () => {
