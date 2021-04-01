8bc5b6ebd8851f7c3270df786156e30ecf2afa2f
Char J <CharJ2889@gmail.com>
2021-04-01 07:59:09 +0100

    animated logo added resized

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, NavLink, Bars, NavMenu, } from './NavbarElements.js';
 
-import logo from "./animationlogo.gif";
+import logo from "./animatedlogo.gif";
 
 
 const Navbar = () => {
