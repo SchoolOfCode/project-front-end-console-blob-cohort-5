8d12fbdf0afadb0d09d42bbbdae117cddc460c8e
Mafe Sanchez <mafesanchez0326@gmail.com>
2021-04-01 15:29:15 +0100

    testing cors

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -4,7 +4,22 @@ import css from "./HomePage.module.css";
 import { useHistory } from 'react-router-dom';
 
 
+
+
 function HomePage() {
+
+  let url= "https://localhost:5001/country";
+  
+   
+      const getData = async () => {
+        let res = await fetch(url);
+        let data = await res.json();
+        
+        console.log(data);
+      };
+      
+
+
 
   const history = useHistory();
 
@@ -12,6 +27,7 @@ function HomePage() {
   <div className={css.main}>
     <Button size="large" text="Traveling Within UK" handleclick={() => history.push('../UKPage')}/>
     <Button size="large" text="Traveling Abroad" handleclick={() => history.push('../WorldPage')}/>
+    <Button handleclick={getData} />
     
   </div> 
   );
