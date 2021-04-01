5ab7d7dca87d73966513f42e198ca81bd2c7c112
Lucas <lucas.beraldin@gmail.com>
2021-04-01 14:42:12 +0100

    slogan and buttons

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from "../ButtonComponent/index";
 import css from "./HomePage.module.css";
 import { useHistory } from 'react-router-dom';
+import logo from './animatedlogo.gif';
 
 
 function HomePage() {
@@ -9,9 +10,19 @@ function HomePage() {
   const history = useHistory();
 
   return (
+
     <div className={css.main}>
-      <Button size="large" text="Traveling Within UK" handleclick={() => history.push('../UKPage')} />
-      <Button size="large" text="Traveling Abroad" handleclick={() => history.push('../WorldPage')} />
+
+      <div className={css.slogan}>
+        <p>Text</p>
+        <img src={logo} />
+      </div>
+
+      <div className={css.buttons}>
+        <Button size="large" text="Traveling Within UK" handleclick={() => history.push('../UKPage')} />
+
+        <Button size="large" text="Traveling Abroad" handleclick={() => history.push('../WorldPage')} />
+      </div>
     </div>
   );
 }
