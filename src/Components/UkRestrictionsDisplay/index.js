1c4f533e609448a11f7f629628c4f2c984f75fbc
craig2812 <craig2812@gmail.com>
2021-04-02 12:06:06 +0100

    UK web scrape connected

--- src/Components/UkRestrictionsDisplay/index.js
+++ src/Components/UkRestrictionsDisplay/index.js
@@ -1,42 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import css from './ukRestDisplay.module.css';
 import cn from 'classnames'
 
 
-url = "/localhost:5001";
 
 function UKRestrictionsDisplay(data, color, size, heading) {
+  let url = "https://localhost:5001/country";
   const [fetchedData, setFetchedData] = useState(null);
-
-  const webScrapeFetch =async () => {
-  
-      let res = await fetch(url);
-      let data = await res.json();
-      setFetchedData(data);
-      console.log(data);
-  }
-
-  let dataUK = {
-    country: "England",
-    covidHeadline: "Stay at Home",
-    quarantineRestrictions: "Don't go out, or 14 day quarantine",
-  };
+  const [click, setClick] = useState(true);
 
 
+  useEffect(() => {
+    const webScrapeFetch =async () => {
+    
+        let res = await fetch(url);
+        let data = await res.json();
+        setFetchedData(data);
+        console.log(data);
+    }
+    webScrapeFetch()
+  }, [click])
+
+//   var acc = document.getElementsByClassName("accordion");
+// var i;
+
+// for (i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var panel = this.nextElementSibling;
+//     if (panel.style.display === "block") {
+//       panel.style.display = "none";
+//     } else {
+//       panel.style.display = "block";
+//     }
+//   });
+// }
+function handleClick(){setClick(!click)}
+
+if (fetchedData){
   return (
    <div className={cn(css[color], css[size])}>
 
      <h2>Current Gov Restrictions for Travel</h2>
      <p>
             {/* this section is webScraped */}
-            County:{dataUK.country}
+            <h2>Travelling within England:</h2>
+            <p>{fetchedData[1].englandInternal.replace(/&#44;/g, ',')}</p>
             <br />
-            covidHeadline: {dataUK.covidHeadline}
+           
+            <h2>Travellling within the UK:</h2>
+           <h3>From England:</h3> <p>{fetchedData[1].toUkFromEngland.replace(/&#44;/g, ',')}</p>
             <br />
-            Restrictions: {dataUK.quarantineRestrictions}{" "}
+            <h3>To England:</h3> <p>{fetchedData[1].toEnglandFromUK.replace(/&#44;/g, ',')}</p>
           </p>
+          {/* <h2>International Travel:</h2>
+          <h3>From England:</h3> <p>{fetchedData[1].inetnationalFromEng.replace(/&#44;/g, ',')}</p>
+            <br />
+            <h3>To England:</h3> <p>{fetchedData[1].toEngfFromInt.replace(/&#44;/g, ',')}</p> */}
+          
    </div>
   );
+}
+return (
+<><h1>Loading...</h1>
+{/* <button onClick={handleClick}>Check UK Gov Restrictions... </button> */}
+</>)
 }
 
 
