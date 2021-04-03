8800f06eef01fe018efef3a1489051fc40e0b02b
craig2812 <craig2812@gmail.com>
2021-04-04 00:32:31 +0100

    more UK page Styling v2

--- src/Components/UKGov API Component/index.js
+++ src/Components/UKGov API Component/index.js
@@ -10,6 +10,7 @@ import {
   PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
+  GridItem,
 } from "@chakra-ui/react";
 
 import increase from "./increase.png";
@@ -18,7 +19,8 @@ import decrease from "./decrease.png";
 
 
 function UkGovApiDisplay({data, data2, search=0}) {
-let color = "blue"
+
+  let color = "blue"
 let covidRate = data[search]?.Rate.PublishDate;
 let covidRatePrev = data2[search]?.Rate.PublishDate ? data2[search]?.Rate.PublishDate : (covidRate-300).toFixed(1)
 let covidChange = ((covidRate / covidRatePrev -1)*100).toFixed(2)
@@ -37,43 +39,59 @@ let covidChangeSign = (covidRate>= 0 ? increase : decrease )
    
     // console.log(data)
   return (
+    <>
    <div className={css.govText}>
-     <p>Cumulative Cases: {data[search]?.cases.cumulative ? data[search]?.cases.cumulative : "No Data for this Area"}</p>
+     <div className={css.rowOne}><h4>Cumulative Cases: {data[search]?.cases.cumulative ? data[search]?.cases.cumulative : "No Data for this Area"}</h4>
+  
+                    {/* <p><span style={{color:"blue"}}>Low  </span><span style={{color:"orange"}}>Medium </span><span style={{color:"red"}}>High</span></p> */}
+                  </div>
+
+       <div className={css.rowTwo}> 
+       <h2>Covid Rates:</h2>
+       <div className={css.popoverButton}>
+                    <Popover width="40%">
+                  <PopoverTrigger>
+                    <div className={css.keyButton} style={{fontWeight:"bolder"}}>?</div>
+                  </PopoverTrigger>
+                  <PopoverContent  bg="white" border="1px solid black" padding="20px"  boxShadow="5px 10px #888888" borderRadius="25px">
+                    <PopoverArrow />
+                    <PopoverCloseButton bg="rgb(59, 182, 155)" borderRadius="25px" width="30px"/>
+                    <PopoverHeader><h2>Covid Rates Key:</h2></PopoverHeader>
+                    <PopoverBody >
+                      Covid Rate is the number of current cases per 100,000 of the area's population.<br/> <br/>
+                      <ul>
+                        <li><span style={{color:"rgb(59, 112, 182)"}}>Low = 5000 and below</span></li> <br/>
+                      <li><span style={{color:"rgb(192, 147, 63)"}}>Medium = 5001 to 8000</span></li> <br/>
+                     <li><span style={{color:" rgb(206, 95, 95)"}}> High = 8001 and Over</span></li> <br/> 
+                      <div style={{fontWeight:'bold'}}><span style={{textDecoration:"underline"
+}}>{data[search]?.areaName}</span>  currently has a Rate of...   <span style={{color:color}}>{covidRate}</span> per 100,000 people</div>
+                      </ul>
+                      </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+                </div>
+                </div>
+
+
      <div className={css.stats}>
-     <div className={css[color]}>
-       <p style={{color:'white'}}>Rate Last Month: {covidRatePrev}</p>
-       </div>
-     <div className={css[color]}>
-       <p style={{color:'white'}}>Rate Today: {covidRate}</p>
-       </div>
-     <div className={css.key}>
-     <Popover width="40%">
-  <PopoverTrigger>
-    <div className={css.keyButton}>???</div>
-  </PopoverTrigger>
-  <PopoverContent  bg="white" color="black" border="1px solid black" padding="20px" borderRadius="25px">
-    <PopoverArrow />
-    <PopoverCloseButton bg="rgb(59, 182, 155)" borderRadius="25px" width="30px"/>
-    <PopoverHeader><h2>Covid Rates...</h2></PopoverHeader>
-    <PopoverBody >
-      Covid Rate is the number of current cases per 100,000 of the area's population.<br/>
-      <span style={{color:"blue", textAlign:"center"}}>Low = 5000 and below</span> <br/>
-      <span style={{color:"orange"}}>Medium = 5001 to 8000</span> <br/>
-      <span style={{color:"red"}}> High = 8001 and Over</span><br/> <br/>
-      <div>{`${data[search]?.areaName} Currently has a Rate of...   `}<span style={{color:color}}>{covidRate}</span> per 100,000 people</div>
-
-      </PopoverBody>
-  </PopoverContent>
-</Popover>
-
-     {/* <p><span style={{color:"blue"}}>Low  </span><span style={{color:"orange"}}>Medium </span><span style={{color:"red"}}>High</span></p> */}
+            <div className={css[color]}>
+                <p style={{color:'white'}}>Last Month: {covidRatePrev}</p>
+              </div>
+     
+            <div className={css[color]}>
+                <p style={{color:'white'}}>Today: {covidRate}</p>
+              </div>
+     
+           
      </div>
-     </div>
-     <p style={{color:colorRate}}>Rate Change since last Month:  {`${covidChange}%`}    <img src={covidChangeSign}></img></p>
-     <p>Last Updated on: {data[search]?.date}</p>
-     {/* <h2>{data[search]?.areaName}</h2> */}
-
    </div>
+     <div className={css[colorRate]}>
+              <p style={{color:'white'}}>Change since last Month:  {`${covidChange}%`}    <img src={covidChangeSign}></img></p>
+              </div>
+
+     <div className={css.update}>Last Updated on: {data[search]?.date}</div>
+     {/* <h2>{data[search]?.areaName}</h2> */}
+   </>
   );
 }
 
