3e1046489bc7a23011f5392354ea74b04b400cf9
craig2812 <craig2812@gmail.com>
2021-04-06 15:35:52 +0100

    finished

--- src/Components/UKGov API Component/index.js
+++ src/Components/UKGov API Component/index.js
@@ -25,18 +25,36 @@ function UkGovApiDisplay({data, data2, search=0}) {
   }
 
   let color = "blue"
+  let colorPrev ="red"
 let covidRate = data[search]?.Rate.PublishDate;
 let covidRatePrev = data2[search]?.Rate.PublishDate ? data2[search]?.Rate.PublishDate : (covidRate-((Math.random()*400)-200)).toFixed(1); 
-let covidChange = ((covidRate / covidRatePrev -1)*100).toFixed(2)
+let population = (data[search]?.cases.cumulative / data[search]?.Rate.PublishDate)*100000
+let actualRate =  data[search]?.cases.daily / population * 100000
+let actualRatePrev =  data2[search]?.cases.daily / population * 100000
+let covidChange = (actualRatePrev - actualRate ).toFixed(2)
+console.log(actualRatePrev)
+// console.log(data2)
+console.log(actualRate)
+console.log(population)
+//cases per 100k 
 
 
-   if (covidRate>5000 && covidRate<8000){
+   if (actualRate>3 && actualRate<=6){
         color="orange"
    }
-   else if (covidRate>=8000){
+   else if (actualRate>6){
     color="red"
-} else if (covidRate<=5000){
+} else if (actualRate<=3){
     color="blue"}
+
+
+if (actualRatePrev>3 && actualRatePrev<=6){
+      colorPrev="orange"
+ }
+ else if (actualRatePrev>6){
+  colorPrev="red"
+} else if (actualRatePrev<=3){
+  colorPrev="blue"}
   
     let colorRate = (covidChange >= 0) ? "redRate" : "greenRate";
    
@@ -51,7 +69,7 @@ let covidChange = ((covidRate / covidRatePrev -1)*100).toFixed(2)
        
                   <h2>Covid Rates:</h2>
 
-                  <Popover width="40%" placement="left" trigger={'hover'} >
+                  <Popover width="40%" placement="left-end" trigger={'hover'} bg="grey">
                   <PopoverTrigger>
                     <div className={css.keyButton} style={{fontWeight:"bolder"}}>?</div>
                   </PopoverTrigger>
@@ -62,12 +80,12 @@ let covidChange = ((covidRate / covidRatePrev -1)*100).toFixed(2)
                     <PopoverBody bg={'white'} >
                       Covid Rate is the number of current cases per 100,000 of the area's population.<br/> <br/>
                       <ul>
-                        <li><span style={{color:"rgb(59, 112, 182)"}}>Low = 500 and below</span></li> <br/>
-                      <li><span style={{color:"#eed202"}}>Medium = 501 to 800</span></li> <br/>
-                     <li><span style={{color:" rgb(206, 95, 95)"}}> High = 801 and Over</span></li> <br/>
+                        <li><span style={{color:"rgb(59, 112, 182)"}}>Low = 3 or Below</span></li> <br/>
+                      <li><span style={{color:"#eed202"}}>Medium = 3.1 to 6 </span></li> <br/>
+                     <li><span style={{color:" rgb(206, 95, 95)"}}> High = 6.1 or More</span></li> <br/>
                      <li>For the Rate Change we have: <span style={{color:" rgb(206, 95, 95)", fontWeight:'bold'}}> RISE</span> and <span style={{color:" rgb(66, 190, 103)", fontWeight:'bold'}}>FALL</span> in the retrospective Covid Rate</li> <br/> 
                       <div style={{fontWeight:'bold'}}><span style={{textDecoration:"underline"
-                      }}>{data[search]?.areaName}</span>  currently has a Rate of...   <span style={{color:color}}>{(covidRate/10).toFixed(2)}</span> per 100,000 people</div>
+                      }}>{data[search]?.areaName}</span>  currently has a Rate of...   <span style={{color:color}}>{(actualRate).toFixed(2)}</span> per 100,000 people</div>
                       </ul>
                       </PopoverBody>
                   </PopoverContent>
@@ -80,19 +98,19 @@ let covidChange = ((covidRate / covidRatePrev -1)*100).toFixed(2)
                 
               <div className={css[colorRate]}>
                  <p style={{color:'white'}}> <h4 >Rate Change</h4> since last Month</p> 
-                  <h5 >{`${covidChange}%`} <img src={((covidChange >= 0) ? increase : decrease )}></img> </h5>
+                  <h5 >{`${covidChange}`} <img src={((covidChange >= 0) ? increase : decrease )}></img> </h5>
                </div>
 
       
 
 
               <div className={css.rates} >
-                  <div className={css[color]}>
-                       <h4 style={{color:'white'}}>Last Month <br/> {(covidRatePrev/10).toFixed(2)}</h4>
+                  <div className={css[colorPrev]}>
+                       <h4 style={{color:'white'}}>Last Month <br/> {actualRatePrev.toFixed(2)}</h4>
                    </div>
      
                   <div className={css[color]}>
-                         <h4 style={{color:'white'}}>Today <br/>{(covidRate/10).toFixed(2)}</h4>
+                         <h4 style={{color:'white'}}>Today <br/>{actualRate.toFixed(2)}</h4>
                   </div>  
               </div>
 
--- src/Components/UKMainPageComponent/index.js
+++ src/Components/UKMainPageComponent/index.js
@@ -18,7 +18,7 @@ function UKPage() {
   DATE2.setMonth(DATE2.getMonth() - 1); //minus mmonth from secodn instance of new date()
   console.log(DATE2.toISOString().substr(0, 10)); //convert back date to readable string
   let URL = `https://api.coronavirus.data.gov.uk//v1/data?filters=date=2021-04-05&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","areaType":"areaType","cases":{"daily":"newCasesByPublishDate","cumulative":"cumCasesByPublishDate"},"deaths":{"daily":"newDeathsByDeathDate","cumulative":"cumDeathsByDeathDate"},"Rate":{"PublishDate":"cumCasesByPublishDateRate"}}`;
-  let URL2 = `https://api.coronavirus.data.gov.uk//v1/data?filters=date=${DATE2.toISOString().substr(0,10)}&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","areaType":"areaType","case s":{"daily":"newCasesByPublishDate","cumulative":"cumCasesByPublishDate"},"deaths":{"daily":"newDeathsByDeathDate","cumulative":"cumDeathsByDeathDate"},"Rate":{"PublishDate":"cumCasesByPublishDateRate"}}`;
+  let URL2 = `https://api.coronavirus.data.gov.uk//v1/data?filters=date=${DATE2.toISOString().substr(0,10)}&structure={"date":"date","areaName":"areaName","areaCode":"areaCode","areaType":"areaType","cases":{"daily":"newCasesByPublishDate","cumulative":"cumCasesByPublishDate"},"deaths":{"daily":"newDeathsByDeathDate","cumulative":"cumDeathsByDeathDate"},"Rate":{"PublishDate":"cumCasesByPublishDateRate"}}`;
 
   useEffect(() => {
     axios
